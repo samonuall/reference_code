39c08c852ef6141aaf8a9ed253ed83190d562749
samonuall <samonuall@gmail.com>
2020-06-27 17:40:39 -0400

    Added in estimated distance data with no obstructions. Also added optional argument p to toggle graphs

--- scan_analyze.py
+++ scan_analyze.py
@@ -5,6 +5,7 @@ and scanner.
 Optional arguments:
 	-Specific csv files to be read
 	-Prefix of csv files to be read
+	-p at END of arguments specifies that each file's RSSI data will be plotted
 	-If no arguments entered, all csv files in directory are analyzed
 """
 from pathlib import Path
@@ -16,7 +17,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-expected_values = {}
+#RSSI baseline: inches between scanner and advertiser. Based off of data with 
+#no obstructions.
+
+expected_values = {-28: '0-10', -35: '10-20', -39: '20-30', -48: '30-40',
+					-58: '40-50', -65: '50-60', -71: '61+'}
 
 def most_frequent(Array, f_count): 
     if len(Array) <= 1:
@@ -31,8 +36,35 @@ def average(array):
 
 
 
-def plot_files(scan_data):
+def estimate_dist(RSSI):
+	for key in expected_values:
+		if RSSI < key:
+			continue
+		else:
+			no_obst_dist = expected_values[key]
+			break
+	else:
+		no_obst_dist = expected_values[key]
+	
+	return no_obst_dist
+		
+
+
+#Plots RSSI data from csv files in scan_data list. Returns a list of the modes
+#for each scan file
+def plot_files(scan_data, plot):
 	plots = []
+	modes = []
+	
+	#If plot option is false, just return modes without plotting graphs
+	if not plot:
+		for file_count, file_name in enumerate(scan_data):
+			data = pd.read_csv(file_name)
+			data = data['RSSI'].to_numpy()
+			mode = most_frequent(data, file_count)
+			modes.append(mode)
+		return modes
+	
 	#create figures each containing at most two graphs
 	for l in range(len(scan_data) // 2):
 		plots.append(plt.subplots(2))
@@ -56,6 +88,7 @@ def plot_files(scan_data):
 			axis.scatter(x_axis, data, marker='o')
 		
 			mode = most_frequent(data, file_count)
+			modes.append(mode)
 			axis.plot(x_axis, [mode]*data.shape[0], linestyle='--',
 					label='Mode = {}'.format(mode))
 		
@@ -70,10 +103,12 @@ def plot_files(scan_data):
 			file_count += 1
 		
 	plt.show()
+	return modes
 	
 
 
 def main():
+	plot = False
 	#Make list scan_data containing str names of each csv file in the directory
 	if(len(sys.argv)) > 1:
 		if '.' in sys.argv[1]:
@@ -93,7 +128,11 @@ def main():
 		scan_data = [str(f) for f in cur_dir.glob('*.csv')]
 		scan_data.sort()
 	
-	plot_files(scan_data)
+	if sys.argv[-1] is 'p':
+		plot = True
+	modes = plot_files(scan_data, plot)
+	for mode in modes:
+		print(estimate_dist(mode))
 
 if __name__ == "__main__":
 	main()
